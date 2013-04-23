d7638e74bcf552b19f6119b8a3ea48a6523357b4
basiclines <ismael@basiclines.com>
2013-04-23 11:24:33 +0200

    Added matchmedia test instead of pure css ones

--- js/tests.js
+++ js/tests.js
@@ -1,13 +1,32 @@
 function min_device_width() {
-	var size = window.matchMedia("(min-device-width: 320px)").matches;
-	for (var i = 0; i < 3000; i++ ){
+	var match = window.matchMedia("(min-device-width: 320px)").matches;
+	var size = 320;
 
-		if (size) {
-			var size = window.matchMedia("(min-device-width: "+i+"px)").matches
-		} else {
-			var matchedSize = i-2;
-			i = 3000;
-		}
+	while (match) {
+		size++;
+		match = window.matchMedia("(min-device-width: "+size+"px)").matches
 	}
-	return matchedSize;
+	return size - 1;
+}
+
+function min_device_pixel_ratio() {
+	var match = window.matchMedia("(min--moz-device-pixel-ratio: 1)").matches;
+	var size = 1;
+
+	while (match) {
+		size++;
+		match = window.matchMedia("min--moz-device-pixel-ratio: "+size+"px)").matches
+	}
+	return size - 1;
+}
+
+function min_resolution() {
+	var match = window.matchMedia("(min-resolution: 96)").matches;
+	var size = 96;
+
+	while (match) {
+		size++;
+		match = window.matchMedia("min-resolution: "+size+"px)").matches
+	}
+	return size;
 }
